a11cd73eacab7c14f174e8b3586bcce640206c33
Kfir-G <kfirg15@gmail.com>
2020-11-03 18:24:58 +0200

    Set only title of meth. addtoTail
    Set only the title+descride of the method: addToTail

--- DDLinkedList.java
+++ DDLinkedList.java
@@ -3,8 +3,8 @@ public class DDLinkedList
 	private ListElement head, tail;
 	
 	/**
-	* Add an element to the head of the doubly-linked-list.
-	* @param val the integer value to be added to the head of the list.
+	* Add an element to the head of the doubly-linked-list
+	* @param val the integer value to be added to the head of the list
 	*/
 	protected void addToHead(int val)
 	{
@@ -17,6 +17,17 @@ public class DDLinkedList
 			tail = newElm;
 			
 		head = newElm;
+	}
+	
+	/**
+	* Add an element to the tail of the doubly-linked-list
+	* @param val the integer value to be added to the tail of the list
+	*/
+	protected void addToTail(int val)
+	{
+		ListElement newElm = new ListElement(val, tail);
+		
+		
 	}
 }
 }
