041b4abde46fb14c1d8ff99353c336aa12b2629c
Kfir-G <kfirg15@gmail.com>
2020-11-04 15:40:51 +0200

    Add Get methods to LE
    - fix constructor of ListElement with 3 parameters.
    - add Get methods to ListElement class

--- DDLinkedList.java
+++ DDLinkedList.java
@@ -1,11 +1,12 @@
 public class DDLinkedList
 {
+	/*//here
 	private ListElement head, tail;
 	
 	/**
 	* Add an element to the head of the doubly-linked-list
 	* @param val the integer value to be added to the head of the list
-	*/
+	*/                                          /* //here
 	protected void addToHead(int val)
 	{
 		ListElement newElm = new ListElement(val, head);
@@ -22,21 +23,23 @@ public class DDLinkedList
 	/**
 	* Add an element to the tail of the doubly-linked-list
 	* @param val the integer value to be added to the tail of the list
-	*/
+	*/                                      /*//here
 	protected void addToTail(int val)
 	{
 		ListElement newElm = new ListElement(val, tail);
 		
 		
-	}
-}
+	} 
+	*/ //here
 }
  class ListElement
 {
+	//---------data fields-----------------
 	private int val;
 	private ListElement next;
 	private ListElement prev;
 	
+	//-------constructors---------------
 	/**
 	* ListElement no-arugment constructor, initializes val data to 0 and 
 	* "next" "prev" references to null
@@ -74,6 +77,29 @@ public class DDLinkedList
 	*/
 	public ListElement (int val, ListElement next, ListElement prev)
 	{
-		this(val,next,prev); // invokes constructor with 3 arguments
+		// invokes constructor with 3 arguments
+		this.val = val;
+		this.next = next;
+		this.prev = prev;
 	}
+	
+	//------------methods----------------
+	/**
+	* get method for the internal data value
+	* @return an <code> integer </code> the value of the list element
+	*/
+	public int getVal()
+	{
+		return val;
+	}
+	
+	/**
+	* get method for the next ListElement reference
+	* @return an <code> ListElement </code> of the next
+	*/
+	public ListElement getNext()
+	{
+		return next;
+	}
+	
 }
